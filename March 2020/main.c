9289188adcafd59dc5460ba013b712317a21c36a
MagicMikeM <62552166+MagicMikeM@users.noreply.github.com>
2020-03-29 20:44:16 +0800

    Mark 3

--- March 2020/main.c
+++ March 2020/main.c
@@ -18,5 +18,58 @@ int main(int argc, const char * argv[]) {
     {
         printf("6");
     }
+    
+    int A[4][4]={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+    int B[5][3]={{4,4,4},{1,1,1},{2,2,6},{3,3,11},{4,4,16}};
+    int C[4][3]={{4,4,3},{2,2,6},{2,3,7},{2,4,8}};
+    int D[3][4]={{1,2,3,4},{5,6,7,8},{9,9,11,12}};
+    int E[4][4]={};
+    
+    printf("A矩阵：\n");
+    for (int i=0; i<=3; i++)
+    {
+        for (int j=0; j<=3; j++)
+            printf("%2d  ",A[i][j]);
+        printf("\n");
+    }
+    printf("A矩阵的对角线元素和为：%d\n",sum(4,A));
+    
+    
+    printf("\nB矩阵：\n");
+    for (int i=0; i<=4; i++)
+    {
+        for (int j=0; j<3; j++)
+            printf("%2d  ",B[i][j]);
+        printf("\n");
+    }
+    printf("C矩阵：\n");
+    for (int i=0; i<=3; i++)
+    {
+        for (int j=0; j<3; j++)
+            printf("%2d  ",C[i][j]);
+        printf("\n");
+    }
+    add(B,C);
+    
+    
+    printf("\nD矩阵：\n");
+    for (int i=0; i<3; i++)
+    {
+        for (int j=0; j<=3; j++)
+            printf("%2d  ",D[i][j]);
+        printf("\n");
+    }
+    max(3, 4, D);
+    printf("鞍点数为：%d\n",saddle_point(3, 4, D));
+    
+    magic_square(E);
+    printf("\nE矩阵：\n");
+    for (int i=0; i<4; i++)
+    {
+        for (int j=0; j<4; j++)
+            printf("%2d ",E[i][j]);
+        printf("\n");
+    }
+
     return 0;
 }
