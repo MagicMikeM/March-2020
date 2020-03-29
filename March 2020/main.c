12b6124bc24ce23c569a6aa634b496ddbb5e68a2
MagicMikeM <62552166+MagicMikeM@users.noreply.github.com>
2020-03-29 19:45:15 +0800

    Initial Commit

---
+++ March 2020/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  March 2020
+//
+//  Created by MagicMikeM on 2020/3/29.
+//  Copyright © 2020 MagicMike. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
