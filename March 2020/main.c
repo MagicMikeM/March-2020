d9e556c783e663d91910509b1a3dc4bc66b50c6f
MagicMikeM <62552166+MagicMikeM@users.noreply.github.com>
2020-03-29 21:24:44 +0800

    Mark 4

--- March 2020/main.c
+++ March 2020/main.c
@@ -7,7 +7,17 @@
 //
 
 #include <stdio.h>
-
+void magic_square(int A[4][4])
+{
+    for (int i=0; i<4; i++)
+        for (int j=1; j<=4; j++)
+            A[i][j-1]=i*4+j;
+    int a;
+    a=A[0][0]; A[0][0]=A[3-0][3-0]; A[3-0][3-0]=a;
+    a=A[1][1]; A[1][1]=A[3-1][3-1]; A[3-1][3-1]=a;
+    a=A[0][3]; A[0][3]=A[3-0][3-3]; A[3-0][3-3]=a;
+    a=A[1][2]; A[1][2]=A[3-1][3-2]; A[3-1][3-2]=a;
+}
 int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Hello, World!\n");
